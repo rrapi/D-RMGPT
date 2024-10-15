a20516fc25a6e70c8657cc9880eb32698d6917f3
rrapi <rrapi.franci@gmail.com>
2024-10-15 16:20:09 +0200

    add simulation test

--- lmm_assembly_task_prova_DEF_simulation.py
+++ lmm_assembly_task_prova_DEF_simulation.py
@@ -71,11 +71,11 @@ while step < num_steps:
 
     t_start_proc		= time.time()
     text_query 			= read_text_file("data/response_assistant.txt")
-    image1			= os.path.join(file_path, f'step_{step+1}_cam1.png')
+    image1			= os.path.join(dataset_path, f'step_{step+1}_cam1.png')
     # Getting the base64 string
     base64_image_current_step1 	= encode_image(image_path_current_step1)
     
-    image2			= os.path.join(file_path, f'step_{step+1}_cam2.png')
+    image2			= os.path.join(dataset_path, f'step_{step+1}_cam2.png')
     # Getting the base64 string
     base64_image_current_step2 	= encode_image(image2)
     
