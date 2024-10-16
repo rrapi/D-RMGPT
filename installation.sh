6ee1a1bcc1a8c4d373d1d75844e692e36cb3b715
rrapi <rrapi.franci@gmail.com>
2024-10-16 15:29:31 +0200

    installation process update

--- installation.sh
+++ installation.sh
@@ -3,8 +3,9 @@
 echo "Installing required libraries..."
 
 sudo apt-get install -y pip
-pip install numpy
 pip install -r requirements.txt
+pip install matplotlib pyrealsense2
+
 export PATH=$HOME/.local/bin:$PATH >> ~/.bashrc
 echo $PATH
 
