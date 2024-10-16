ebe48327fcdf6c96a4a50af5e17545952b094e38
rrapi <rrapi.franci@gmail.com>
2024-10-16 15:45:23 +0200

    installation process update

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 # Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
 # and rename this file to constants.py.
 APIKEY="replace here"
-
+GITHUB_TOKEN="replace here"
+MODEL_NAME="replace here"
