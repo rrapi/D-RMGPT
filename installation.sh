65954a956f463152e029355e1360ea496b0b273b
rrapi <rrapi.franci@gmail.com>
2024-10-16 14:49:07 +0200

    simulation files improvement

--- installation.sh
+++ installation.sh
@@ -1,3 +1,11 @@
-sudo apt-get install pip
+#!/bin/bash
+
+echo "Installing required libraries..."
+
+apt-get install -y pip
 pip install -r requirements.txt
 export PATH=$USER/.local/bin:$PATH >> ~/.bashrc
+
+
+
+echo "Done!"
