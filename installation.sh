200e04bc0d80715f92da02bb1357c56dfe00eb79
rrapi <rrapi.franci@gmail.com>
2024-10-16 15:40:53 +0200

    installation process update

--- installation.sh
+++ installation.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# ======== To run the script execute:
+# > chmod +x installation.sh
+# > ./intallation.sh <USER> (in this case `robot`)
+
+
+
+
 echo "Installing required libraries..."
 
-sudo apt-get install -y pip
+sudo apt-get install -y --no-install-recommends pip sox
 pip install -r requirements.txt
 pip install matplotlib pyrealsense2
 
 export PATH=$HOME/.local/bin:$PATH >> ~/.bashrc
 echo $PATH
+
+sudo usermod -aG audio "$1"
 
 echo "Done!"
